a3820c83be8b70f5ae802643ae1e5ec1e1451a62
Ben Visser <benny@noqcks.io>
2017-06-20 21:55:39 -0400

    updating ping file

--- ping/ping.py
+++ ping/ping.py
@@ -5,11 +5,9 @@ import os
 from urllib2 import Request,urlopen
 
 # global constants
-EXTERNAL_ENDPOINTS = {'us-east1': '35.196.13.205', 'us-west1': '35.185.246.194', 'asia-east-1':'35.185.137.79', 'asia-northeast1':'35.189.139.104', 'asia-southest1':'35.187.232.105', 'europe-west1':'104.199.46.155', 'europe-west2': '35.189.70.26', 'us-central1':'130.211.145.80', 'us-east4': '35.186.181.15'}
+EXTERNAL_ENDPOINTS = {'us-east1': '35.196.13.205', 'us-west1': '35.185.246.194', 'asia-east1':'35.185.137.79', 'asia-northeast1':'35.189.139.104', 'asia-southeast1':'35.187.232.105', 'europe-west1':'104.199.46.155', 'europe-west2': '35.189.70.26', 'us-central1':'130.211.145.80', 'us-east4': '35.186.181.15'}
 INTERNAL_ENDPOINTS = {}
 METADATA_ENDPOINT = 'http://metadata.google.internal/computeMetadata/v1/instance/zone'
-
-
 r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 
 
@@ -17,7 +15,7 @@ r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT
 def ping_region(endpoint, times=10, timeout=2, psize=64):
   latency = []
   for _ in range(times):
-    latency.append(np.round(pyping.do_one(endpoint, timeout, psize)*1000), 2)
+    latency.append(pyping.do_one(endpoint, timeout, psize)*1000)
   return np.mean(latency)
 
 # return the current region that the instance is
