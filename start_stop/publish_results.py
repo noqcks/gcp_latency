b6eb5e3684d59470c0237bb90c0d05f8b6d5bc83
Ben Visser <benny@noqcks.io>
2017-06-20 21:56:27 -0400

    updating the way I publish the results to google cloud

--- start_stop/publish_results.py
+++ start_stop/publish_results.py
@@ -4,43 +4,55 @@ import time
 import googleapiclient.discovery
 import http
 import os
+import pandas as pd
+import numpy as np
 from StringIO import StringIO
 
 
-# JSON structure that is created
-# {
-#    "datetime": "<datetime>",
-#    "latencies": {
-#       "<region_from>": {
-#          "<region_to>": "<latency>"
-#       }
-#    }
-# }
-
-REGIONS = ['us-east1', 'us-west1', 'asiaeast--1', 'asia-northeast1', 'asia-southest1', 'europe-west1', 'europe-west2', 'us-central1', 'us-east4']
+REGIONS = ['us-east1', 'us-west1', 'asia-east1', 'asia-northeast1', 'asia-southeast1', 'europe-west1', 'europe-west2', 'us-central1', 'us-east4']
 r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 
 def main():
-  data = {}
-  data['latencies'] = {}
-  for region in REGIONS:
-    latencies = r.hgetall(region)
-    data['latencies'][region] = latencies
+  my_df = []
+  for source_region in REGIONS:
+    latencies = r.hgetall(source_region)
 
-  data['datetime'] = time.strftime("%Y-%m-%d %H:%M")
-  upload_to_google_storage(data)
+    d = {}
+    d['region'] = source_region
+
+    for dest_region, latency in latencies.iteritems():
+      d[dest_region] = latency
+      my_df.append(d)
+
+  my_df = pd.DataFrame(my_df)
+  my_df = my_df.convert_objects(convert_numeric=True)
+  my_df = my_df.round(2)
+  my_df.drop_duplicates(inplace=True)
+  my_df = my_df.set_index('region')
+  my_df.sort_index(axis=1, inplace=True)
+
+  # fill diagonal with 0s
+  my_df = my_df.reindex(columns=list(my_df.index) + list(my_df.columns.difference(my_df.index)))
+  np.fill_diagonal(my_df.values,0)
+
+  # convert to string
+  my_df.to_csv('data.csv')
+  s = StringIO()
+  my_df.to_csv(s)
+
+  upload_to_google_storage(s)
 
 def upload_to_google_storage(data):
   service = googleapiclient.discovery.build('storage', 'v1')
   bucket_name = 'gcp-latency'
-  media = googleapiclient.http.MediaIoBaseUpload(StringIO(json.dumps(data)), mimetype='plain/text')
+  media = googleapiclient.http.MediaIoBaseUpload(data, mimetype='plain/text')
 
   req = service.objects().insert(
     bucket=bucket_name,
     body={"cacheControl": "public,max-age=31536000"},
     media_body=media,
     predefinedAcl='publicRead',
-    name='latency_results.json',
+    name='latencies.csv',
   )
   resp = req.execute()
 
