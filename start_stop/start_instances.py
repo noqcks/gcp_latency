babf2cff8136021dcb1e9fc9ff9ff7a4aea154d1
Ant Kenworthy <mcrmonkey@users.noreply.github.com>
2018-05-22 12:53:50 +0100

    Add Additional zones in other regions

--- start_stop/start_instances.py
+++ start_stop/start_instances.py
@@ -4,12 +4,16 @@ compute = googleapiclient.discovery.build('compute', 'v1')
 
 compute.instances().start(project='noqcks', instance='us-east-1-ping', zone='us-east1-c').execute()
 compute.instances().start(project='noqcks', instance='asia-east1-ping', zone='asia-east1-a').execute()
+compute.instances().start(project='noqcks', instance='asia-south1-c', zone='asia-south1-c').execute()
 compute.instances().start(project='noqcks', instance='asia-northeast1-ping', zone='asia-northeast1-a').execute()
 compute.instances().start(project='noqcks', instance='asia-southeast1-ping', zone='asia-southeast1-a').execute()
 compute.instances().start(project='noqcks', instance='europe-west1-ping', zone='europe-west1-d').execute()
 compute.instances().start(project='noqcks', instance='europe-west2-ping', zone='europe-west2-b').execute()
+compute.instances().start(project='noqcks', instance='europe-west3-c', zone='europe-west3-c').execute()
 compute.instances().start(project='noqcks', instance='us-central1-ping', zone='us-central1-c').execute()
 compute.instances().start(project='noqcks', instance='us-east4-a', zone='us-east4-a').execute()
 compute.instances().start(project='noqcks', instance='us-west-1', zone='us-west1-b').execute()
+compute.instances().start(project='noqcks', instance='australia-southeast1-c', zone='australia-southeast1-c').execute()
+compute.instances().start(project='noqcks', instance='southamerica-east1-c', zone='southamerica-east1-c').execute()
 
 print "started instances"
